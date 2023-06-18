5a41b10a8917c587223eabed8471dbd6cc8385b3
GodyRacks <godfreymatagaro@gmail.com>
2023-06-18 17:25:28 +0300

    Andika Notes CRUD RESTful App

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -2,8 +2,21 @@ $("#add_note").submit(function(event){
   alert("Data Inserted Successfully!")
 });
 
+function toggleTheme() {
+  const body = document.body;
+  const themeToggle = document.getElementById('theme-toggle');
+  const currentTheme = body.getAttribute('data-theme');
 
-  
+  if (currentTheme === 'light') {
+    body.setAttribute('data-theme', 'dark');
+    themeToggle.innerHTML = '<i class="material-icons">brightness_5</i>';
+    body.classList.add('dark-theme');
+  } else {
+    body.setAttribute('data-theme', 'light');
+    themeToggle.innerHTML = '<i class="material-icons">brightness_4</i>';
+    body.classList.remove('dark-theme');
+  }
+}
   
   
   // Toggle Theme, script.js
@@ -16,4 +29,13 @@ $("#add_note").submit(function(event){
   });
 
   //
+  window.addEventListener("scroll", function() {
+    var header = document.getElementById("app-header");
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+  
   
